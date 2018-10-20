d08701fc9168973d4f123a11a545694a035a59af
hmesander <haleymesander@gmail.com>
2018-10-19 20:06:33 -0600

    Adds more optimized solution for addends problem

--- tech-challenges/addends.rb
+++ tech-challenges/addends.rb
@@ -10,14 +10,19 @@
 # return [0, 1].
 
 def two_sum(nums, target)
-  hash = Hash[nums.zip(0...nums.size)]
-  nums.each do |first_num|
-    nums.each do |second_num|
-      if first_num + second_num == target && hash[first_num] != hash[second_num]
-        return [hash[first_num], hash[second_num]]
+  index1 = 0
+  index2 = index1 + 1
+  until index1 == (nums.size - 1)
+    until index2 == nums.size
+      if nums[index1] + nums[index2] == target
+        return [index1, index2]
       end
+      index2 += 1
     end
+    index1 += 1
+    index2 = index1 + 1
   end
 end
 
 print two_sum([10, 2, 11, 16], 27)
+print two_sum([31, 0, 12, 8], 12)
